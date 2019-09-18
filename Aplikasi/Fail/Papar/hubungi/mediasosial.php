be5decac6267a18ac8cdbd2ed751af7c65c9d7aa
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-18 14:31:10 +0800

    tambah fail

---
+++ Aplikasi/Fail/Papar/hubungi/mediasosial.php
@@ -0,0 +1,50 @@
+<h1><a class="btn btn-outline-secondary btn-lg butangBulat" href="<?php echo URL ?>">
+<small>Kembali ke pangkal jalan</small></a></h1>
+
+<hr>
+<h1 class="text-center">&nbsp;<span class="bg-secondary butangBulat">&nbsp;<?php echo $this->Tajuk_Muka_Surat; ?>&nbsp;</span></h1>
+<div class="row">
+	<div class="col-lg-3">&nbsp;</div>
+	<div class="col-lg-6">
+		<blockquote>
+			<a target="_blank" href="https://www.facebook.com/amin007.ledang" class="btn btn-secondary butangBulat">
+				<i class="fab fa-facebook"></i>: amin007.ledang</a>
+			<a target="_blank" href="https://www.twitter.com/amin007_ledang" class="btn btn-secondary butangBulat">
+				<i class="fab fa-twitter-square"></i>: amin007_ledang</a>
+			<a target="_blank" href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary butangBulat">
+				<i class="fab fa-linkedin"></i>: amin007ledang</a>
+			<a target="_blank" href="https://github.com/amin007" class="btn btn-secondary butangBulat">
+				<i class="fab fa-github"></i>: amin007</a>
+			<a target="_blank" href="https://instagram.com/aminledang" class="btn btn-secondary butangBulat">
+				<i class="fab fa-instagram"></i>: aminledang</a>
+			<a class="btn btn-secondary text-white butangBulat">
+				<i class="fab fa-snapchat">amin007ledang</i></a>
+			<!-- a target="_blank" href="#" class="btn btn-secondary butangBulat">
+				<i class="fab fa-skype"></i>: amin800507</a>
+			<a target="_blank" href="#" class="btn btn-secondary butangBulat">
+				<i class="fab fa-youtube"></i>: user/abdmuhaimin</a -->
+		</blockquote>
+	</div><!-- / class="col-lg-6" -->
+</div><!-- / class="row" -->
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="row"><div class="col-lg-6">
+</div><!-- / class="col-lg-6" -->
+</div><!-- / class="row" -->
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="container">
+	<ul class="list-group list-group-flush">
+	<li class="list-group-item"><span class="btn btn-outline-dark">Ruangan Iklan</span></li>
+	<li class="list-group-item">
+		<!-- iklan google -->
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({
+				google_ad_client: "ca-pub-2307262272744070",
+				enable_page_level_ads: true
+			});
+		</script>
+		<!-- iklan google -->
+	</li>
+	</ul>
+</div><!-- / class="container" -->
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->

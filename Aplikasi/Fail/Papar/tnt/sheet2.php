cb4ea713f78a05eb18fc832e2f0b4fb7cb4399cd
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-18 15:53:06 +0800

    buat template dulu

---
+++ Aplikasi/Fail/Papar/tnt/sheet2.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<style type="text/css">
+/* ********************************************************************* */
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+/* ********************************************************************* */
+</style>
+</head>
+<body>
+
+<h1> Keterangan </h1>
+
+<!-- untuk javascrip
+=========================================================================== -->
+</body>
+</html>

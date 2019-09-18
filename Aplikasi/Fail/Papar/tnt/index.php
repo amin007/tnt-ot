fbbc4ef2fd6dc5439cfaead79c4b0601e3eb63ed
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-18 15:38:10 +0800

    tambah fail Papar untuk tnt

---
+++ Aplikasi/Fail/Papar/tnt/index.php
@@ -0,0 +1,50 @@
+<h1><span class="bg-secondary text-white rounded-pill">&nbsp;Pengenalan&nbsp;</span></h1>
+<div class="container">
+<div class="row">
+<div class="col">
+	<h1 style="text-align:center;background-color:white;">Tentang TNT dan OT<img
+		src="http://via.placeholder.com/93x101" alt="logo"
+		width="93" height="101"></h1>
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
+			<a class="btn btn-outline-secondary btn-lg rounded-pill" href="<?php echo URL ?>tnt/biodata">
+				Biodata</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/1">
+				Cetak Muka1</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/2">
+				Cetak Muka2</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/3">
+				Cetak Muka3</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/4">
+				Cetak Muka4</a>
+		</div>
+		<div class="col-sm-8" style="background-color:white;text-align:justify;">
+			<h2>Assalamualaikum &amp; Selamat Datang Ke <br> Dunia Programming Saya</h2>
+			<p>Nama saya Amin, berasal dari muar, johor.
+			Laman web ini menceritakan tentang aplikasi <b> TNT dan OT</b>
+			<br><br>
+			Sekian Terima Kasih. Wasalam</p>
+		</div><!-- / class="col-sm-7" -->
+		<div class="col-sm-1"><?php
+$namaServer = $_SERVER['SERVER_NAME'];
+if (($namaServer=='localhost') || ($namaServer=='amindell') ):
+	echo null;
+else:?>
+			<!-- iklan google -->
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({
+					google_ad_client: "ca-pub-2307262272744070",
+					enable_page_level_ads: true
+				});
+			</script>
+			<!-- iklan google -->
+<?php
+endif;
+?>
+		</div><!-- / class="col-sm-1" -->
+	</div><!-- / class="row" -->
+</div><!-- / class="col" -->
+</div><!-- / class="row" -->
+</div><!-- / class="container" -->

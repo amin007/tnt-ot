0882fd2a8bb3630940b4406745ec5e98d3f101c1
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-19 14:00:51 +0800

    tambah tatasusunan

--- Aplikasi/Kelas/Utama/Tanya/tnt_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/tnt_tanya.php
@@ -30,8 +30,31 @@ class Tnt_Tanya extends \Aplikasi\Kitab\Tanya
 		//list($a1,$a2,$a3) = dpt_senarai('TNT_PEGAWAI');
 		$data = array(
 			'namaPegawai' => 'POLAN BIN POLAN',
+			'alamatBertugas' => 'UNIT RISIKAN KHAS',
+			'noAkaun' => '987456321',
+			'namaBank' => 'Maybank',
+			'alamatBank' => 'Jalan Kaya Raya',
+			'noInvois' => 'URKTNT-09/19-POLAN',
 			'noKP' => '010203010123',
-			'alamatBertugas' => 'UNIT RISIKAN KHAS'
+			'noPembekal' => '20190103',
+			'noGaji' => '5020190103',
+			'noTelefon' => '0123456799',
+			'email' => 'awak@duduk.mana',
+			'gred' => 'N19',
+			'kumpulan' => 'SOKONGAN',
+			'jawatan' => 'PEMBANTU TADBIR',
+			'gajiPokok' => '1234.00',
+			'elaun' => '656.00',
+			'jumGaji' => '1890.00',
+			'alamatPejabat01' => '***',
+			'alamatPejabat02' => '***',
+			'alamatPejabat03' => '***',
+			'alamatPejabat04' => '***',
+			'alamatRumah01' => '***',
+			'alamatRumah02' => '***',
+			'alamatRumah03' => '***',
+			'alamatRumah04' => '***',
+			//'***' => ''
 		); # dapatkan medan terlibat
 		$kira = 1; # kira jumlah data
 

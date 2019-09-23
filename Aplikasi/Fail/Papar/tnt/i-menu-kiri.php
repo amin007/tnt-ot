bb1bbf3286232648d1b6ccd6510f6d0bb04d4dfc
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-23 08:33:26 +0800

    buat fail baru untuk menu kiri

---
+++ Aplikasi/Fail/Papar/tnt/i-menu-kiri.php
@@ -0,0 +1,16 @@
+			<a class="btn btn-outline-secondary btn-lg rounded-pill" href="<?php echo URL ?>tnt">
+				Pengenalan</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/biodata">
+				Biodata</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/borang/keterangan">
+				Keterangan Tuntutan</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/borang/formula">
+				Formula</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/1">
+				Cetak Muka1</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/2">
+				Cetak Muka2</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/3">
+				Cetak Muka3</a>
+			<a class="btn btn-outline-secondary rounded-pill" href="<?php echo URL ?>tnt/cetak/4">
+				Cetak Muka4</a>

85419c61b84428bed0ac9a7e902b2c4a765132a8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-29 18:59:01 +0800

    buat fungsi borangBaru()

--- Aplikasi/Kelas/Utama/Kawal/tnt.php
+++ Aplikasi/Kelas/Utama/Kawal/tnt.php
@@ -88,6 +88,27 @@ class Tnt extends \Aplikasi\Kitab\Kawal
 		$this->paparKandungan($this->_folder,$f[$p],$noInclude=0);//*/
 	}
 #------------------------------------------------------------------------------------------
+	public function borangBaru($a=null,$b=null)
+	{
+		echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = $a;
+		//$this->papar->senarai['biodata'][] = $this->tanya->biodata();
+		/*list($t, $m, $c, $s, $p) = $this->tanya->cariKeterangan($b);
+		list($data,$meta) = $this->tanya->cariSemuaDataMeta//cariSql
+			($t, $m, $c, $s, $p);
+		$this->papar->senarai['keterangan'] = $data;
+		$this->papar->_meta = $meta;//*/
+
+		# Pergi papar kandungan
+		$p = 2; $f = array('index','biodata','keterangan','formula');
+		$this->semakPembolehubah($_POST,'POST');# Semak data dulu
+		$this->semakPembolehubah($b,'b');# Semak data dulu
+		//$this->semakPembolehubah($this->papar->senarai,'senarai');# Semak data dulu
+		//$this->semakPembolehubah($this->papar->_meta,'_meta');# Semak data dulu
+		//$this->paparKandungan($this->_folder,$f[$p],$noInclude=0);//*/
+	}
+#------------------------------------------------------------------------------------------
 	public function borangUbah($a=null,$b=null)
 	{
 		echo '<hr>Nama class :' . __METHOD__ . '<hr>';

f98ddced7d74ddd031592a460f2a0027e3888155
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-29 17:54:49 +0800

    tambah ubahBorang

--- Aplikasi/Kelas/Utama/Kawal/tnt.php
+++ Aplikasi/Kelas/Utama/Kawal/tnt.php
@@ -88,6 +88,25 @@ class Tnt extends \Aplikasi\Kitab\Kawal
 		$this->paparKandungan($this->_folder,$f[$p],$noInclude=0);//*/
 	}
 #------------------------------------------------------------------------------------------
+	public function ubahBorang($a=null,$b=null)
+	{
+		echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = $a;
+		//$this->papar->senarai['biodata'][] = $this->tanya->biodata();
+		/*list($t, $m, $c, $s, $p) = $this->tanya->cariKeterangan($b);
+		list($data,$meta) = $this->tanya->cariSemuaDataMeta//cariSql
+			($t, $m, $c, $s, $p);
+		$this->papar->senarai['keterangan'] = $data;
+		$this->papar->_meta = $meta;//*/
+
+		# Pergi papar kandungan
+		$p = 2; $f = array('index','biodata','keterangan','formula');
+		$this->semakPembolehubah($this->papar->senarai,'senarai');# Semak data dulu
+		$this->semakPembolehubah($this->papar->_meta,'_meta');# Semak data dulu
+		//$this->paparKandungan($this->_folder,$f[$p],$noInclude=0);//*/
+	}
+#------------------------------------------------------------------------------------------
 	public function cetak($p=1)
 	{
 		//echo '<hr>Nama class :' . __METHOD__ . '<hr>';

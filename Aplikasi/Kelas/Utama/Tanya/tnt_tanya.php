965b73ae938d4b97b6e19fecc35c3c34c9563eff
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-29 15:31:50 +0800

    pilih nama jadual

--- Aplikasi/Kelas/Utama/Tanya/tnt_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/tnt_tanya.php
@@ -162,5 +162,23 @@ class Tnt_Tanya extends \Aplikasi\Kitab\Tanya
 		return ($pilih==1) ? $kira : $data; # pulangkan nilai
 	}
 #------------------------------------------------------------------------------------------------#
+	public function keterangan()
+	{
+		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		//list($idUser,$namaPendek) = $this->tanyaDataSesi();
+		$table = dpt_senarai('jadual_nama');
+		$t = $table[1];// nama table = t
+		$m = '*';// nama medan = m
+		$c = $s = $p = null;// carian = c | susun = s | pencam = p
+		# semak table
+		/*$c[] = array('fix' => 'x=', # cari x= / %like% / xlike
+			'atau' => 'WHERE', # WHERE / OR / AND
+			'medan' => 'level', # cari dalam medan apa
+			'apa' => 'user'); # benda yang dicari//*/
+		# pilih pembolehubah = p
+		//$p[] = null;
+		return array($t, $m, $c, $s, $p); # pulangkan nilai
+	}
+#------------------------------------------------------------------------------------------------#
 #=================================================================================================
 }

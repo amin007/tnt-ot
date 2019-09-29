dfd2d3f7f13189df474ce9e9f7eb820febe383a7
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-29 12:56:33 +0800

    tukar butangBulat kepada rounded-pill

--- Aplikasi/Fail/Papar/hubungi/mediasosial.php
+++ Aplikasi/Fail/Papar/hubungi/mediasosial.php
@@ -1,27 +1,27 @@
-<h1><a class="btn btn-outline-secondary btn-lg butangBulat" href="<?php echo URL ?>">
+<h1><a class="btn btn-outline-secondary btn-lg rounded-pill" href="<?php echo URL ?>">
 <small>Kembali ke pangkal jalan</small></a></h1>
 
 <hr>
-<h1 class="text-center">&nbsp;<span class="bg-secondary butangBulat">&nbsp;<?php echo $this->Tajuk_Muka_Surat; ?>&nbsp;</span></h1>
+<h1 class="text-center">&nbsp;<span class="bg-secondary rounded-pill">&nbsp;<?php echo $this->Tajuk_Muka_Surat; ?>&nbsp;</span></h1>
 <div class="row">
 	<div class="col-lg-3">&nbsp;</div>
 	<div class="col-lg-6">
 		<blockquote>
-			<a target="_blank" href="https://www.facebook.com/amin007.ledang" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="https://www.facebook.com/amin007.ledang" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-facebook"></i>: amin007.ledang</a>
-			<a target="_blank" href="https://www.twitter.com/amin007_ledang" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="https://www.twitter.com/amin007_ledang" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-twitter-square"></i>: amin007_ledang</a>
-			<a target="_blank" href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-linkedin"></i>: amin007ledang</a>
-			<a target="_blank" href="https://github.com/amin007" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="https://github.com/amin007" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-github"></i>: amin007</a>
-			<a target="_blank" href="https://instagram.com/aminledang" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="https://instagram.com/aminledang" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-instagram"></i>: aminledang</a>
-			<a class="btn btn-secondary text-white butangBulat">
+			<a class="btn btn-secondary text-white rounded-pill">
 				<i class="fab fa-snapchat">amin007ledang</i></a>
-			<!-- a target="_blank" href="#" class="btn btn-secondary butangBulat">
+			<!-- a target="_blank" href="#" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-skype"></i>: amin800507</a>
-			<a target="_blank" href="#" class="btn btn-secondary butangBulat">
+			<a target="_blank" href="#" class="btn btn-secondary rounded-pill">
 				<i class="fab fa-youtube"></i>: user/abdmuhaimin</a -->
 		</blockquote>
 	</div><!-- / class="col-lg-6" -->

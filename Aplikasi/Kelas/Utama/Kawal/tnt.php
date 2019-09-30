0cf18ef7534ef19b10c2148195dd1a5a941b1ef0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-30 19:00:33 +0800

    tutup debug

--- Aplikasi/Kelas/Utama/Kawal/tnt.php
+++ Aplikasi/Kelas/Utama/Kawal/tnt.php
@@ -69,9 +69,9 @@ class Tnt extends \Aplikasi\Kitab\Kawal
 		$this->papar->senarai['keterangan'] = $data;
 		$this->papar->_meta = $meta;//*/
 		# Semak data dulu
-		$this->semakPembolehubah($data,'data');
+		//$this->semakPembolehubah($data,'data');
 		//$this->semakPembolehubah($this->papar->senarai,'senarai');
-		$this->semakPembolehubah($this->papar->_meta,'_meta');
+		//$this->semakPembolehubah($this->papar->_meta,'_meta');
 	}
 ##------------------------------------------------------------------------------------------
 	function logout()

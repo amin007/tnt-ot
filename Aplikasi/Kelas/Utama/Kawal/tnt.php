26fb3894a0f4251fa64926449138e1444dde007f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-30 18:25:26 +0800

    tutup komen

--- Aplikasi/Kelas/Utama/Kawal/tnt.php
+++ Aplikasi/Kelas/Utama/Kawal/tnt.php
@@ -169,7 +169,7 @@ class Tnt extends \Aplikasi\Kitab\Kawal
 	public function borangUbah($a=null,$b=null)
 	{
 		echo '<hr>Nama class :' . __METHOD__ . '<hr>';
-		$this->semakPembolehubah($a,'a');# Semak data dulu
+		//$this->semakPembolehubah($a,'a');# Semak data dulu
 		//$this->semakPembolehubah($b,'b');# Semak data dulu
 		# Set pemboleubah utama
 		$this->papar->Tajuk_Muka_Surat = $a;

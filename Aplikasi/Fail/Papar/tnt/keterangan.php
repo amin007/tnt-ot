c69fb3f3b58839965c5b9fb1fd132a23aeff4078
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-10-01 08:25:15 +0800

    tambah fungsi paparMedan() dan paparInputData()

--- Aplikasi/Fail/Papar/tnt/keterangan.php
+++ Aplikasi/Fail/Papar/tnt/keterangan.php
@@ -284,6 +284,24 @@ END;
 		return array($tx,$semua);
 	}
 #-------------------------------------------------------------------------------------------------
+	function paparMedan($key,$tx,$class2)
+	{
+		$k0 = $key;
+		//$k0 = "$key|$tx";
+		echo  "\n" . '<div class="form-group row">' . "\n\t"
+		. '<label for="inputTajuk" class="col-sm-2 control-label text-right">'
+		. $k0 . '</label>' . "\n\t" . '<div class="'.$class2.'">';
+		#
+	}
+#-------------------------------------------------------------------------------------------------
+	function paparInputData($paparData,$class2)
+	{
+		echo $paparData . "\n\t"
+		. '</div><!-- / class="' . $class2 . '" -->' . "\n"
+		. '</div><!-- / class="form-group" -->' . "\n";
+		#
+	}
+#-------------------------------------------------------------------------------------------------
 	function ulangJadualAsal($jadual,$baris)
 	{
 		#

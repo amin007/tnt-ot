8cbeca83bb7836f8b33dcaeb87581410e57eb5c2
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-10-04 17:52:00 +0800

    tambah jenis type of data

--- Aplikasi/Fungsi/Fungsi.php
+++ Aplikasi/Fungsi/Fungsi.php
@@ -218,6 +218,40 @@ function papar_jadual($row, $myTable, $pilih)
 }
 // tamat untuk kod php+html 
 #------------------------------------------------------------------------------------------
+/**
+ * Returns the type of the var passed.
+ *
+ * @param mixed $var Variable
+ * @return string Type of variable
+ */
+function myGetType($var)
+{
+	if (is_array($var)) return "array";
+	if (is_bool($var)) return "boolean";
+	if (is_float($var)) return "float";
+	if (is_int($var)) return "integer";
+	if (is_null($var)) return "NULL";
+	if (is_numeric($var)) return "numeric";
+	if (is_object($var)) return "object";
+	if (is_resource($var)) return "resource";
+	if (is_string($var)) return "string";
+	return "unknown type";
+}
+#------------------------------------------------------------------------------------------
+function phpToSqlDataType($var)
+{
+	if (is_array($var)) return 'enum';//"array";
+	if (is_bool($var)) return 'tinyint';//"boolean";
+	if (is_float($var)) return 'float';//"float";
+	if (is_int($var)) return 'int';//"integer";
+	if (is_null($var)) return 'varchar';//"NULL";
+	//if (is_numeric($var)) return "numeric";
+	//if (is_object($var)) return "object";
+	//if (is_resource($var)) return "resource";
+	if (is_string($var)) return 'varchar';//"string";
+	return 'apakah!!!';
+}
+#------------------------------------------------------------------------------------------
 function kira($kiraan)
 {
 	// pecahan kepada ratus
